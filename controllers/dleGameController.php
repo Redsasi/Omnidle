f9832bb1d2fb9edd8acbd8128f2a0034632563be
Saïam Cañedo <redgames3000@gmail.com>
2024-11-24 17:09:19 +0100

    ajout de certain commentaire

--- controllers/dleGameController.php
+++ controllers/dleGameController.php
@@ -10,6 +10,7 @@ class dleGameController {
         $this->model = new DleGameModel();
     }
 
+    //affiche l'entete suivant si il est connecter
     public function displayHeader(){
         if(isset($_SESSION["userId"])){
             require '../view/layout/headerLogin.php';
@@ -26,10 +27,15 @@ class dleGameController {
     }
 
 
+    //creation d'un nouveau quizz
     public function creat(){
+        if(isset($_SESSION['userId'])){//si l'utilisateur n'est pas connecter affiche la page de connexion
+            header("Location: ". URL_USER_LOGIN);
+            exit();
+        }
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $image = null;
-            if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+            if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {//ajout l'image si elle est donnée
                 $imageFile = $_FILES['image']['tmp_name'];
                 $image = file_get_contents($imageFile);
             }
@@ -44,6 +50,7 @@ class dleGameController {
         }
     }
 
+    //affichage des quizz d'un utilisateur
     public function displayQuizzUser(){
         if(isset($_SESSION['userId'])){
             $this->displayHeader();
@@ -53,24 +60,29 @@ class dleGameController {
             header("Location: ". URL_USER_LOGIN);
         }
     }
+
+    //modification d'un quizz
     public function updateQuizz(){
-        if(!isset($_GET['quizzId']) && empty($_GET['quizzId'])){
+        if(!isset($_GET['quizzId']) && empty($_GET['quizzId'])){//retour a l'affichage si aucun quizz n'est donnée
             header("Location: ". URL_QUIZZES_DISPLAY_USER);
             exit();
         }
         
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if(isset($_POST['name']) || !empty($_POST['name']) ||
-            isset($_POST['description']) || !empty($_POST['description'])){
+            isset($_POST['description']) || !empty($_POST['description'])){ // verification de la validité des donnée
+
                 $name = $_POST['name'];
                 $description = $_POST['description'];
-                if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+
+                if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {//modifie l'image si elle est donnée avec
                     $imageFile = $_FILES['image']['tmp_name'];
                     $image = file_get_contents($imageFile);
                     $this->model->updateByIdImage($_GET['quizzId'], $name, $image, $description);
                 }else{
                     $this->model->updateById($_GET['quizzId'], $name, $description);
                 }
+
                 header("Location: ". URL_QUIZZES_DISPLAY_USER);
                 exit();
             }
@@ -81,16 +93,17 @@ class dleGameController {
         require '../view/dleGame/quizzUpdate.php';
     }
 
+    //suppression d'un quiz
     public function deleteQuizz(){
         if(!isset($_GET['quizzId']) && empty($_GET['quizzId'])){
             header("Location: ". URL_QUIZZES_DISPLAY_USER);
             exit();
         }
         $quizz = $this->model->readQuizzById($_GET['quizzId']);
-        if(isset($_SESSION['userId']) && $_SESSION['userId'] == $quizz['USER_ID']){
+        if(isset($_SESSION['userId']) && $_SESSION['userId'] == $quizz['USER_ID']){//refu si l'utilisateur n'est pas le createur du quizz
             $this->model->deletById($_GET['quizzId']);
         }else{
-            echo "ERROR you don't own this quizz";
+            echo "error you don't own this quizz";
             exit();
         }
         header("Location: ". URL_QUIZZES_DISPLAY_USER);
